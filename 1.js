106d03717f25e546b5a131ef0dd6e349a89f6112
asp.net <1277887838@qq.com>
2016-08-17 10:28:14 +0800

    1

---
+++ 1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/8/4 0004.
+ */
+
+Array.prototype.unique5 = function() {
+    var res = [], hash = {};
+    for(var i=0, elem; (elem = this[i]) != null; i++)  {
+        if (!hash[elem])
+        {
+            res.push(elem);
+            hash[elem] = true;
+        }
+    }
+    return res;
+}
